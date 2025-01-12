b25776d9c1aaadf23e71db4304f6ffab5142d9f4
Myles Gaerlan <ish.myles@gmail.com>
2025-01-12 17:55:11 +1100

    feat: set up ejs for web app
    Users can view frontend pages received by the server

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import messageRouter from "./routes/messagesRouter.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
 app.use("/messages", messageRouter);
 
 app.get("/", (req, res) => {
-  res.send("HOME PAGE");
+  res.render("index.ejs");
 });
 
 app.listen(PORT, () => console.log(`Server now listening at port ${PORT}`));
